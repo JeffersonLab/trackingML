582b5786318553149aa502fb42da5102da89fd6d
gavalian <gavalian@jlab.org>
2019-02-12 14:42:06 -0500

    trying to build convolutional network

--- clas12/training/src/main/java/org/jlab/ml/tracking/nn/RunTraining.java
+++ clas12/training/src/main/java/org/jlab/ml/tracking/nn/RunTraining.java
@@ -1,5 +1,10 @@
 package org.jlab.ml.tracking.nn;
 
+import java.util.List;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.zoo.ZooModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,5 +18,15 @@ package org.jlab.ml.tracking.nn;
 public class RunTraining {
     public static void main(String[] args){
         System.out.println("running ML tracking learning algorithm...");
+        
+        TrackingModel model = new TrackingModel();
+        MultiLayerConfiguration   config = model.getConfiguration();
+        MultiLayerNetwork        network = new MultiLayerNetwork(config);
+        
+        List<String> layerNames = network.getLayerNames();
+        for(int i = 0; i < layerNames.size(); i++){
+            System.out.println(String.format("%3d : %12s", i, layerNames.get(i)));
+        }
+        //ZooModel zooModel = ;
     }
 }
---
+++ clas12/training/src/main/java/org/jlab/ml/tracking/nn/TrackingModel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jlab.ml.tracking.nn;
+
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+
+/**
+ *
+ * @author gavalian
+ */
+public class TrackingModel {
+ 
+    public MultiLayerConfiguration getConfiguration(){
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(12345)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .list().build();
+        return conf;
+    }
+    
+}
