2eeef86b84fca6ef2a333d6c9650dea12d175f1d
David Lawrence <davidl@jlab.org>
2019-03-31 08:25:38 -0400

    Add installation of CUDA. (untested)

--- benchmark/GCP_install.sh
+++ benchmark/GCP_install.sh
@@ -7,6 +7,17 @@
 # The user should be able to run sudo without a password.
 #
 
-sudo install python3 gcc-g++
-sudo pip3 install keras tensorflow-gpu pandas matplotlib imutils pillow scikit-learn opencv-python
+# Install CUDA
+sudo yum install -y wget
+wget https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.1.105-1.x86_64.rpm
+sudo rpm -i cuda-repo-*.rpm
+sudo yum install -y cuda
+
+# Install python3 and C++ compiler
+sudo yum install python3 gcc-g++
+
+# Create virtual python environment and install needed packages into it
+pyvenv venv
+source venv/bin/activate
+sudo pip install keras tensorflow-gpu pandas matplotlib imutils pillow scikit-learn opencv-python
 
